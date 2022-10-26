6ac012b9f275b05321baeef353416cdb67431386
shellyguns <talleyranv@gmail.com>
2022-10-27 00:29:42 +0300

    created a submission form and handling for it

---
+++ meetups/forms.py
@@ -0,0 +1,9 @@
+from dataclasses import fields
+from django import forms
+
+from meetups.models import Participant
+
+class RegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Participant
+        fields = ['email']
--- meetups/views.py
+++ meetups/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from meetups.forms import RegistrationForm
+
 from .models import Meetup
 
 # Create your views here.
@@ -13,10 +15,19 @@ def index(request):
 def meetup_details(request, meetup_slug):
     try:
         selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        if request.method == 'GET':
+            registration_form = RegistrationForm()
+        else:
+            registration_form = RegistrationForm(request.POST)
+            if registration_form.is_valid():
+                participant = registration_form.save()
+                selected_meetup.participants.add(participant)
+                
         return render(request, 'meetups/meetup-detail.html', {
-            'meetup_found': True,
-            'meetup': selected_meetup
-        })
+                'meetup_found': True,
+                'meetup': selected_meetup,
+                'form': registration_form
+            })
     except Exception as exception:
         return render(request, 'meetups/meetup-detail.html', {
             'meetup_found': False
